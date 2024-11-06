62a9dc1f81b153cab1ea37ea8ae0e2582ade802b
rallmac <tobijahekperikpe@gmail.com>
2024-11-06 06:48:56 +0100

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -31,7 +31,7 @@ def get_locale():
 @app.route('/')
 def index():
     """This is the route of the app"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
