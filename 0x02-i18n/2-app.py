99ecd12fb8f206fbc0fba48955e4ff02de143aaf
rallmac <tobijahekperikpe@gmail.com>
2024-11-06 06:45:18 +0100

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""This is a flask app that prints hello world
+   and the heading is welcome to holberton
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """Configuration for Flask app and Babel."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# Set up the app config
+app.config.from_object(Config)
+
+# Instantiate the Babel object
+babel = Babel(app)
+
+
+def get_locale():
+    """Select the best match for supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """This is the route of the app"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
