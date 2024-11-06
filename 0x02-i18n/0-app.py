5a47d0fe1d9b0d8c0d4f7e436568404bb7f8932a
rallmac <tobijahekperikpe@gmail.com>
2024-11-06 03:49:45 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """This is the route of the app"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
